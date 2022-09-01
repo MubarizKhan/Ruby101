0f57f9aac9cf8a96d6f9374f98a736555f97c04e
MubarizKhan <p176007@nu.edu.pk>
2022-09-01 14:42:15 +0500

    adding strings

---
+++ strings.rb
@@ -0,0 +1,18 @@
+"""
+A String object in Ruby holds and manipulates an arbitrary sequence of one or more bytes,
+typically representing characters that represent human language.
+
+The simplest string literals are enclosed in single quotes (the apostrophe character).
+ The text within the quote marks is the value of the string −
+
+
+The backslash also works to escape another backslash,
+so that the second backslash is not itself interpreted as an escape character.
+Following are the string-related features of Ruby."""
+
+
+puts 'This is a simple Ruby string literal'
+
+# If you need to place an apostrophe within a single-quoted string literal,
+# precede it with a backslash, so that the Ruby interpreter does not think that it terminates the string −
+puts 'Won\'t you read O\'Reilly\'s book?'
