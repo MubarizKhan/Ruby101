f6f21f06939d76af7ca18141b4223902ced72c10
MubarizKhan <p176007@nu.edu.pk>
2022-09-01 13:22:30 +0500

    adding module files

---
+++ addModule.rb
@@ -0,0 +1,7 @@
+module add
+  a = 10
+  b = 10
+  def add.sum(a,b)
+    a + b
+  end
+end
---
+++ modules.rb
@@ -0,0 +1,30 @@
+"""
+https://www.tutorialspoint.com/ruby/ruby_modules.htm
+
+Modules are a way of grouping together methods, classes, and constants.
+Modules give you two major benefits.
+
+    Modules provide a namespace and prevent name clashes.
+    Modules implement the mixin facility.
+
+Modules define a namespace, a sandbox in which your methods and
+constants can play without having to worry about being stepped on by other methods and constants.
+
+Syntax
+
+module Identifier
+   statement1
+   statement2
+   ...........
+end
+
+Module constants are named just like class constants, with an initial uppercase letter.
+The method definitions look similar, too: Module methods are defined just like class methods.
+As with class methods, you call a module method by preceding its name with the modules
+ name and a period, and you reference a constant using the module name and two colons
+
+ Modules define a namespace,
+ a sandbox in which your methods and constants can play without having to worry
+ about being stepped on by other methods and constants.
+ """
+
